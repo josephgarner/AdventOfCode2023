38fc1c8b695b35e39725f536b93ec8b6125cd806
Jo Garner <jogarner@Jos-MacBook-Air.local>
2023-12-03 07:57:40 +1100

    day One complete

--- DayOne/puzzle_Two.js
+++ DayOne/puzzle_Two.js
@@ -3,7 +3,7 @@ const readline = require("readline");
 
 const inputs = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
 
-async function solution() {
+async function solutionTwo() {
   const numbers = new Set(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]);
   const wordNumbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
   let total = 0;
@@ -21,8 +21,8 @@ async function solution() {
     let cabValueLast = null;
 
     let word = "";
-
     let index = 0;
+
     //Get First
     while (cabValueFirst == null) {
       if (numbers.has(line.charAt(index))) {
@@ -40,12 +40,15 @@ async function solution() {
 
       index++;
     }
-    console.log(cabValueFirst);
+    // console.log(cabValueFirst);
 
+    // Reset Values
     index = line.length;
     word = "";
 
+    // Get Last
     while (cabValueLast == null) {
+      // Check if number
       if (numbers.has(line.charAt(index))) {
         cabValueLast = line.charAt(index);
       }
@@ -53,6 +56,8 @@ async function solution() {
       word += line.charAt(index);
 
       // console.log(word);
+
+      //Reverse current word and check if it is a digit
       wordNumbers.forEach((substring, index) => {
         if (word.split("").reverse().join("").includes(substring)) {
           cabValueLast = String(index);
@@ -60,63 +65,69 @@ async function solution() {
       });
       index--;
       if (index == 0) {
-        cabValueLast = "";
+        cabValueLast = cabValueFirst;
       }
     }
-    console.log(cabValueFirst + cabValueLast);
+    console.log(cabValueFirst, cabValueLast);
     total += Number(cabValueFirst + cabValueLast);
   }
-  console.log(total);
+  console.log("Solution Two:", total);
 }
 
-// async function solution() {
-//   const numbers = new Set(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]);
-//   const wordNumbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-//   let total = 0;
+async function solutionOne() {
+  const numbers = new Set(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]);
+  const wordNumbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  let total = 0;
 
-// const fileStream = fs.createReadStream("inputTwo.txt");
+  const fileStream = fs.createReadStream("inputTwo.txt");
 
-// const rl = readline.createInterface({
-//   input: fileStream,
-//   crlfDelay: Infinity,
-// });
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
 
-// for await (const line of rl) {
-// console.log(`Line from file: ${line}`);
-// let allNumbers = [];
-// let word = "";
+  for await (const line of rl) {
+    // console.log(`Line from file: ${line}`);
+    let allNumbers = [];
+    let word = "";
 
-//     for (let i = 0; i < line.length; i++) {
-//       word += line.charAt(i);
+    for (let i = 0; i < line.length; i++) {
+      word += line.charAt(i);
 
-//       if (numbers.has(line.charAt(i))) {
-//         allNumbers.push(line.charAt(i));
-//         word = "";
-//       }
+      if (numbers.has(line.charAt(i))) {
+        allNumbers.push(line.charAt(i));
+        word = "";
+      }
 
-//       if (word.length >= 3) {
-//         wordNumbers.forEach((substring, index) => {
-//           if (word.includes(substring)) {
-//             allNumbers.push(String(index));
-//             i -= 1;
-//             word = "";
-//           }
-//         });
-//       }
-//       console.log(word);
-//     }
+      if (word.length >= 3) {
+        wordNumbers.forEach((substring, index) => {
+          if (word.includes(substring)) {
+            allNumbers.push(String(index));
+            i -= 1;
+            word = "";
+          }
+        });
+      }
+      // console.log(word);
+    }
 
-//     console.log(allNumbers);
+    // console.log(allNumbers);
 
-//     if (allNumbers.length == 1) {
-//       console.log(allNumbers[0]);
-//       total += Number(allNumbers[0]);
-//     } else {
-//       console.log(allNumbers[0] + allNumbers[allNumbers.length - 1]);
-//       total += Number(allNumbers[0] + allNumbers[allNumbers.length - 1]);
-//     }
-//   }
-//   console.log(total);
-// }
+    if (allNumbers.length == 1) {
+      // console.log(allNumbers[0]);
+      total += Number(allNumbers[0] + allNumbers[0]);
+    } else {
+      // console.log(allNumbers[0] + allNumbers[allNumbers.length - 1]);
+      total += Number(allNumbers[0] + allNumbers[allNumbers.length - 1]);
+    }
+  }
+  console.log("Solution One:", total);
+}
 
-solution();
+solutionOne();
+
+// solutionTwo();
+
+//56315
+
+//56324
